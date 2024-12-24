d5c323b2d4d6bb9a194a42c91832e0fc79714cdd
Andrew Shell <andrew@andrewshell.org>
2024-12-23 19:11:58 -0600

    Converting to 11ty and making linktree homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@ import formsPlugin from "@tailwindcss/forms";
 import typographyPlugin from "@tailwindcss/typography";
 
 export default {
-  content: ['./*.html'],
-    safelist: ['ml-error'],
+  content: [
+    './_includes/**/*.{html,njk}',
+    './content/**/*.{html,njk}',
+  ],
+  safelist: ['ml-error'],
   theme: {
     extend: {},
   },
